9a8d4dfbb5845c4721d27ed79dde94e2ca3ea092
proyectodamjr <jr.proyectodam@gmail.com>
2024-06-05 22:34:36 +0200

    Carga de trabajo diaria - 2024-06-05

--- server/server.js
+++ server/server.js
@@ -212,3 +212,23 @@ app.get('/api/videos', async (req, res) => {
         return res.status(500).json({ message: "Error en el servidor." });
     }
 });
+
+
+// Ruta para eliminar un video por su id
+app.delete('/eliminarVideo', async (req, res) => {
+    //const videoId = req.params.id;
+    try {
+        // Realizar la eliminación del video con el id proporcionado
+        const [result] = await pool.query('DELETE FROM videos WHERE id = 1');
+        if (result.affectedRows === 1) {
+            // Si se eliminó correctamente
+            return res.status(200).json({ message: "Video eliminado correctamente." });
+        } else {
+            // Si no se encontró el video con el id proporcionado
+            return res.status(404).json({ message: "No se encontró el video con el ID proporcionado." });
+        }
+    } catch (error) {
+        console.error('Error al eliminar el video:', error);
+        return res.status(500).json({ message: "Error en el servidor." });
+    }
+});
